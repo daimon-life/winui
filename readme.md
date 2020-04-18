d5219b26489365b6666a47e19c955df00f174a4b
robb_win <364886923@qq.com>
2020-04-18 17:19:37 +0800

    finis update_duilib.bat

--- readme.md
+++ readme.md
@@ -2,6 +2,8 @@
 
 #### uisystem
 
+##### 树型结构
+
 * [_duilib](https://github.com/duilib/duilib)
 * [_DuiLib_Ultimate](https://github.com/qdtroy/DuiLib_Ultimate)
 * [_NIM_Duilib_Framework](https://github.com/netease-im/NIM_Duilib_Framework/)
@@ -19,3 +21,22 @@
 另外
 
 * siui
+
+##### 文件说明
+
+update_duilib.bat
+
+```bash
+cd uisystem
+# 更新所有duilib
+update_duilib.bat
+# 更新duilib_src
+update_duilib.bat duilib_src
+# 更新duilib_qdtroy
+update_duilib.bat duilib_src
+# 更新duilib_nim
+update_duilib.bat duilib_nim
+# 更新duilib_tim
+update_duilib.bat duilib_tim
+```
+
--- uisystem/update_duilib.bat
+++ uisystem/update_duilib.bat
@@ -2,18 +2,48 @@
 
 
 rem duilib
+:duilib_src
+if not exist _duilib\.git git submodule update --init -- _duilib
+if exist _duilib\.git git pull origin
+if %errorlevel% NEQ 0 goto endduilib_src
 if exist duilib_src rd /s /q duilib_src
 xcopy /e .\_duilib                                           .\duilib_src\
+if exist duilib_src\.git erase duilib_src\.git
+:endduilib_src
 
-rem TIMSDK
-if exist duilib_tim rd /s /q duilib_tim
-xcopy /e .\_TIMSDK\cross-platform\Windows\IMApp\Basic\duilib .\duilib_tim\
-
-rem NIM_Duilib_Framework
-if exist duilib_nim rd /s /q duilib_nim
-xcopy /e .\_NIM_Duilib_Framework\duilib                      .\duilib_nim\
 
 rem DuiLib_Ultimate
-if exist duilib_qdtroy rd /s /q duilib_src
+:duilib_qdtroy
+if not exist _DuiLib_Ultimate\.git git submodule update --init -- _DuiLib_Ultimate
+if exist _DuiLib_Ultimate\.git git pull origin
+if %errorlevel% NEQ 0 goto endduilib_qdtroy
+if exist duilib_qdtroy rd /s /q duilib_qdtroy
 xcopy /e .\_DuiLib_Ultimate                                  .\duilib_qdtroy\
+if exist duilib_qdtroy\.git erase duilib_qdtroy\.git
+:endduilib_qdtroy
 
+
+rem NIM_Duilib_Framework
+:duilib_nim
+if not exist _NIM_Duilib_Framework\.git git submodule update --init -- _NIM_Duilib_Framework
+if exist _NIM_Duilib_Framework\.git git pull origin
+if %errorlevel% NEQ 0 goto endduilib_nim
+if exist duilib_nim rd /s /q duilib_nim
+xcopy /e .\_NIM_Duilib_Framework\duilib                      .\duilib_nim\
+if exist duilib_nim\.git erase duilib_nim\.git
+:endduilib_nim
+
+
+rem TIMSDK
+:duilib_tim
+if not exist _TIMSDK\.git git submodule update --init -- _TIMSDK
+if exist _TIMSDK\.git git pull origin
+if %errorlevel% NEQ 0 goto endduilib_tim
+if exist duilib_tim rd /s /q duilib_tim
+xcopy /e .\_TIMSDK\cross-platform\Windows\IMApp\Basic\duilib .\duilib_tim\
+if exist duilib_tim\.git erase duilib_tim\.git
+:endduilib_tim
+
+
+rem end
+:end
